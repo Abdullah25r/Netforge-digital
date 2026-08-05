33c2998e8e5d18be59cce424fa2fe7668d4f2313
Abdullah bhatti <bhatti253r@gmail.com>
2026-08-05 15:48:29 +0500

    fix: remote patterns

--- next.config.ts
+++ next.config.ts
@@ -3,14 +3,18 @@ import { withPayload } from '@payloadcms/next/withPayload'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Your Next.js config here
-  images: {
+  
     remotePatterns: [
       {
         protocol: "https",
         hostname: "images.unsplash.com",
       },
+      {
+        protocol: "https",
+        hostname: ".public.blob.vercel-storage.com"
+      }
     ],
-  },
+  
   cacheComponents: true,
 }
 
